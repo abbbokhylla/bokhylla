078ee3f6271e4b3f95f404aded553f0a1350dee3
ABBJamMon <54663152+ABBJamMon@users.noreply.github.com>
2020-02-04 14:52:59 +0100

    databas

---
+++ src/components/funfacts.vue
@@ -0,0 +1,21 @@
+<template>
+   <v-btn @click="fetch"> </v-btn>
+
+</template>
+
+<script>
+const axios = require('axios').default;
+export default {
+methods:{
+    async fetch(){
+        const{data}=await axios.get("https://shy0h1cogf.execute-api.us-east-1.amazonaws.com/Beta/funfact");
+        /*eslint-disable*/
+        console.log(data)
+  }
+ }
+}
+</script>
+
+<style>
+
+</style>
